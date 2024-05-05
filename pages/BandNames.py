5ada446ef77db9b6f70bab48e9c3d2917fac1b37
vjchristopher <vjchristopher@yahoo.com>
2024-05-05 22:59:41 +0530

    Update BandNames.py

--- pages/BandNames.py
+++ pages/BandNames.py
@@ -1,8 +1,7 @@
 import streamlit as st 
 import pandas as pd
 
-st.icons("table")
-st.header('Different Band plans used by the Telecom Operators in India:', divider='blue')
+st.header('📈 Different Band plans used by the Telecom Operators in India:', divider='blue')
 
 band_names=pd.read_csv("band_names.csv")
 band_names.index += 1
