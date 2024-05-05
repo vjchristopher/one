7e39116ea8fec5b3a7081d4876997fe6bb40956b
vjchristopher <vjchristopher@yahoo.com>
2024-05-05 22:50:31 +0530

    Update BandNames.py

--- pages/BandNames.py
+++ pages/BandNames.py
@@ -4,5 +4,6 @@ import pandas as pd
 st.header('🔊 Different Band plans used by the Telecom Operators in India:', divider='blue')
 
 band_names=pd.read_csv("band_names.csv")
+band_names.index += 1
 st.table(band_names)
 
