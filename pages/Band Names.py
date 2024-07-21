38a3c93806ff13a5820ae58943e4c19173f0a5b0
vjchristopher <vjchristopher@yahoo.com>
2024-07-21 23:04:42 +0530

    Delete Band Names.py

--- pages/Band Names.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-import pandas as pd
-
-st.header('📝 Different Band plans used by the Telecom Operators in India:', divider='blue')
-
-band_names=pd.read_csv("band_names.csv")
-band_names.index += 1
-st.table(band_names)
-
