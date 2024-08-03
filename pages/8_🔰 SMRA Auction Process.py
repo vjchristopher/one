fd01300d043a86fa826d2bd0426a341106c9153c
vjchristopher <vjchristopher@yahoo.com>
2024-08-03 22:55:56 +0530

    Delete 8_🔰 SMRA Auction Process.py

--- pages/8_🔰 SMRA Auction Process.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-# first,  last = st.columns([90,10])
-# with first:
-st.title("The same SMRA Algorithm is used in all spectrum auctions")
-st.image('SMRA.png')
-    
-st.divider()
