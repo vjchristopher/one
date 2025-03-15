c5cdc736551e6f3d1ce737e0a587e23c9708f612
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2025-03-15 22:45:03 +0530

    Delete pages/9_🔰 SMRA Auction Process.py

--- pages/9_🔰 SMRA Auction Process.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-# first,  last = st.columns([90,10])
-# with first:
-st.title("⏲️The same SMRA Algorithm has been used in all spectrum auctions")
-st.image('SMRA.png')
-    
-st.divider()
